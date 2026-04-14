03d62d9c93096178c6e6f21c171f0bab3238ca21
MD KHALEED AHMED <khalidahmad1598@gmail.com>
2026-04-14 11:11:23 +0530

    Update server.js

--- NotesHub/backend/server.js
+++ NotesHub/backend/server.js
@@ -30,7 +30,7 @@ app.use('/api/ratings', ratingRoutes);
 
 // Connect config
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI 
+const MONGO_URI = process.env.MONGO_URI ;
     // || 'mongodb://127.0.0.1:27017/noteshub';
 
 mongoose.connect(MONGO_URI)
